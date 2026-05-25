4b1542dfd15fd48d463af332b49b5047ad1ec6d4
amoreCaro <admin@MacBook-Pro-3.local>
2026-05-25 20:13:38 +0300

    post-card small

---
+++ src/js/components/slider.js
@@ -0,0 +1,38 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+export function slider() {
+  document.querySelectorAll('.modal-cart__slider').forEach((el) => {
+    const parent = el.closest('.post-card');
+
+    const nextBtn = parent.querySelector('.modal-cart__next');
+    const prevBtn = parent.querySelector('.modal-cart__prev');
+    const pagination = parent.querySelector('.modal-cart__pagination');
+
+    const swiper = new Swiper(el, {
+      modules: [Navigation, Pagination],
+      loop: false,
+      slidesPerView: 1,
+
+      navigation: {
+        nextEl: nextBtn,
+        prevEl: prevBtn,
+      },
+
+      pagination: {
+        el: pagination,
+        clickable: true,
+      },
+
+      on: {
+        init: (swiper) => {
+          updateButtons(swiper, prevBtn, nextBtn);
+        },
+
+        slideChange: (swiper) => {
+          updateButtons(swiper, prevBtn, nextBtn);
+        },
+      },
+    });
+  });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import { slider } from "./components/slider.js";
+import { postVideo } from "./components/postVideo.js";
 import { estimateSinglePostReadTime } from "./components/estimateSinglePostReadTime.js";
 import { calculateTotalPages } from "./components/calculateTotalPages.js";
 import { burgerMenu } from "./components/burgerMenu.js";
@@ -14,6 +16,8 @@ import { signInit } from './components/form/sign.js';
 
 
 document.addEventListener('DOMContentLoaded', function() {
+  slider();
+  postVideo();
   lazyLoadImages();
   video();
   themeToggle();
