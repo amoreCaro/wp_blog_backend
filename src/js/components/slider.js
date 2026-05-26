721ebc9d7db909ece5b0d5374135fbc99bb0c299
amoreCaro <admin@MacBook-Pro-3.local>
2026-05-26 10:53:02 +0300

    feat banner block

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -2,12 +2,12 @@ import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 
 export function slider() {
-  document.querySelectorAll('.modal-cart__slider').forEach((el) => {
+  document.querySelectorAll('.slider').forEach((el) => {
     const parent = el.closest('.post-card');
 
-    const nextBtn = parent.querySelector('.modal-cart__next');
-    const prevBtn = parent.querySelector('.modal-cart__prev');
-    const pagination = parent.querySelector('.modal-cart__pagination');
+    const nextBtn = parent.querySelector('.slider__btn-next');
+    const prevBtn = parent.querySelector('.slider__btn-prev');
+    const pagination = parent.querySelector('.slider__pagination');
 
     const swiper = new Swiper(el, {
       modules: [Navigation, Pagination],
